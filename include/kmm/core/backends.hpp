c09189acd29d8aa4caccd552dc3900772b806c8b
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2026-03-05 13:18:03 +0100

    Typo.

--- include/kmm/core/backends.hpp
+++ include/kmm/core/backends.hpp
@@ -260,7 +260,7 @@ using GPUmemAllocationHandleType = hipMemAllocationHandleType;
 using GPUmemLocationType = hipMemLocationType;
 using GPUevent_t = hipEvent_t;
 using GPU_MEMCPY2D = hip_Memcpy2D;
-using gpuMemcpyDeviceToHost = cudaMemcpyDeviceToHost;
+using gpuMemcpyDeviceToHost = hipMemcpyDeviceToHost;
 
     // cuBLAS
     #define blasCreate                                    rocblas_create_handle
